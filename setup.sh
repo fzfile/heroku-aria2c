4e4a21f268620bb15c9479410f98289e5e50fd45
your_name <your_email>
2020-05-26 07:54:12 +0200

    change aria2 to new one
    change

--- setup.sh
+++ setup.sh
@@ -6,8 +6,7 @@ unzip -q rclone-v1.51.0-linux-amd64.zip
 export PATH=$PWD/rclone-v1.51.0-linux-amd64:$PATH
 
 # Install aria2c static binary
-wget -q https://github.com/q3aql/aria2-static-builds/releases/download/v1.35.0/aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
-tar xf aria2-1.35.0-linux-gnu-64bit-build1.tar.bz2
+wget -q https://github.com/fzfile/heroku-aria2c/raw/master/Aria2/aria2c
 export PATH=$PWD/aria2-1.35.0-linux-gnu-64bit-build1:$PATH
 
 # Create download folder
