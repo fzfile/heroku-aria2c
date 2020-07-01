ed2afcb66def83b3ca7e54c9c30bfda493abf9a5
fzfile <60556447+fzfile@users.noreply.github.com>
2020-07-01 05:24:04 +0200

    keep folder structure

--- on-complete.sh
+++ on-complete.sh
@@ -4,11 +4,6 @@ filePath=$3
 relativePath=${filepath#./downloads/}
 topPath=./downloads/${relativePath%%/*} # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
 
-LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
-FONT_COLOR_SUFFIX="\033[0m"
-INFO="[${LIGHT_GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
-
-echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file ..."
 
 if [ $2 -eq 0 ]; then
     exit 0
@@ -17,13 +12,6 @@ elif [ -e "${filepath}.aria2" ]; then
 elif [ -e "${topPath}.aria2" ]; then
     rm -vf "${topPath}.aria2"
 fi
-echo -e "$(date +"%m/%d %H:%M:%S") ${INFO} Delete .aria2 file finish"
-echo "$(($(cat numUpload)+1))" > numUpload # Plus 1
 
-if [[ $2 -eq 1 ]]; then # single file
-	rclone -v --config="rclone.conf" move "$3" "DRIVE:$RCLONE_DESTINATION" 2>&1	
-elif [[ $2 -gt 1 ]]; then # multiple file
-	rclone -v --config="rclone.conf" move "$topPath" "DRIVE:$RCLONE_DESTINATION/${relativePath%%/*}"
-fi
+rclone -v --config="rclone.conf" move "$topPath" "DRIVE:$RCLONE_DESTINATION/${relativePath%%/*}"
 
-echo "$(($(cat numUpload)-1))" > numUpload # Minus 1
