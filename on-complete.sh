b7788835d20269bdf179cb2499e4cf6b909f18da
fzfile <fzfileserver@gmail.com>
2020-07-03 23:56:03 +0200

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 filePath=$3
-relativePath=${filepath#downloads/}
-topPath=downloads/${relativePath%%/*} # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
+realativepath=$(expr "$filepath" : 'downloads\/\(.*\)')
+toppath=downloads/$(expr "$realativepath" : '\(.*?\)\/') # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
 
 echo $filePath
 echo $relativePath
 echo $topPath 
-echo ${relativePath%/*}
 
 # LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
 # FONT_COLOR_SUFFIX="\033[0m"
