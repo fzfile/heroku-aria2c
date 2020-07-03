26fd4b97356cd3726f4dbff99590fa97218e3c13
fzfile <fzfileserver@gmail.com>
2020-07-03 21:12:26 +0200

    Update on-complete.sh

--- on-complete.sh
+++ on-complete.sh
@@ -4,6 +4,11 @@ filePath=$3
 relativePath=${filepath#./downloads/}
 topPath=./downloads/${relativePath%%/*} # It will be the path of folder when it has multiple files, otherwise it will be the same as file path.
 
+echo $filePath
+echo $relativePath
+echo $topPath
+echo $relativePath%/*
+
 LIGHT_GREEN_FONT_PREFIX="\033[1;32m"
 FONT_COLOR_SUFFIX="\033[0m"
 INFO="[${LIGHT_GREEN_FONT_PREFIX}INFO${FONT_COLOR_SUFFIX}]"
