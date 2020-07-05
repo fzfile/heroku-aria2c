aec3f085bd0bb1f5a861648f173190b485b77689
fzfile <fzfileserver@gmail.com>
2020-07-05 12:52:36 +0200

    fix error

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 wget -q https://github.com/donwa/gclone/releases/download/v1.51.0-mod1.3.1/gclone_1.51.0-mod1.3.1_Linux_x86_64.gz
 gunzip gclone_1.51.0-mod1.3.1_Linux_x86_64.gz
 mkdir -p gclone
-mv gclone_1.51.0-mod1.3.1_Linux_x86_64 gclone/gclone_1.51.0-mod1.3.1_Linux_x86_64
+mv gclone_1.51.0-mod1.3.1_Linux_x86_64 gclone/gclone
 chmod +x gclone/gclone_1.51.0-mod1.3.1_Linux_x86_64
 export PATH=$PWD/gclone:$PATH
 
