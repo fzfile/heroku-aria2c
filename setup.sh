f04ee2ab99286d9dfadc975a0163b34bf110b8ab
fzfile <fzfileserver@gmail.com>
2020-07-05 13:07:09 +0200

    test

--- setup.sh
+++ setup.sh
@@ -32,8 +32,7 @@ unzip -q SA.zip
 # gclone configration
 if [[ -n $RCLONE_CONFIG && -n $RCLONE_DESTINATION ]]; then
 	echo "Rclone config detected"
-	echo -e "[DRIVE]\n$RCLONE_CONFIG" > rclone.conf
-	echo "service_account_file_path = ./SA/" > rclone.conf
+	echo -e "[DRIVE]\n$RCLONE_CONFIG\nservice_account_file_path = ./SA/" > rclone.conf
 	echo "on-download-stop=./delete.sh" >> aria2c.conf
 	echo "on-download-complete=./on-complete.sh" >> aria2c.conf
 	chmod +x delete.sh
